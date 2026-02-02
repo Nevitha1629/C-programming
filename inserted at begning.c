b488e1ef0ce98f27e9ab91d9f3207ff9d72b6374
Nevitha1629 <nevithasathiyamoorthi@gmail.com>
2026-02-02 10:29:02 +0530

    added files

---
+++ inserted at begning.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct contact{ 
+long int number;
+struct contact *next;
+};
+int main(){
+    //insert at benging
+    struct contact *head = NULL;
+    struct contact *stn1 = (struct contact*) malloc(sizeof(struct contact));
+    struct contact *stn2 = (struct contact*) malloc(sizeof(struct contact));
+    struct contact *stn3 = (struct contact*) malloc(sizeof(struct contact));
+    struct contact *stn4 = (struct contact*) malloc(sizeof(struct contact));
+    
+    stn1->number = 9487176095;
+    stn2->number = 9600856797;
+    stn3->number = 9944054068;
+    stn4->number = 8300884068;
+    //INSERTING 
+    stn1->next = head;
+    head = stn1;
+    stn2->next = head;
+    head = stn2;
+    stn3->next = head;
+    head = stn3;
+    stn4->next = head;
+    head = stn4;
+    
+   
+    printf("contact:\n");
+    while(head !=NULL){
+        printf("%ld \n", head->number);
+        head = head->next;
+    }
+    printf("Finish");
+    
+    return 0;
+}
